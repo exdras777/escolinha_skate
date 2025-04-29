ed1f96d27d524c0c7e0c786a7c9873f1bbb5216f
exdras777 <esdras475r@gmail.com>
2025-04-29 19:57:05 -0400

    Configura basename do BrowserRouter para GitHub Pages
    Atualiza o src/index.js para envolver o App com BrowserRouter
    e define o basename para corresponder ao nome do repositório
    no GitHub Pages (/escolinha-skate-react).
    
    Também atualiza as dependências no package.json e package-lock.json
    (possivelmente após instalar o react-router-dom).

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// Importa o CSS do Bootstrap
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Importa os ícones do Bootstrap (opcional, mas útil)
-import 'bootstrap-icons/font/bootstrap-icons.css';
-// Importa seu CSS customizado (DEPOIS do Bootstrap para sobrescrever)
-import './styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/escolinha-skate-react">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
